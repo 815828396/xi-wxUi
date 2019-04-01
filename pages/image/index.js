4a12246f7f8a5f565ad4a89fe47eec24b5ac4877
815828396 <815828396@qq.com>
2019-04-01 13:57:20 +0800

    U:修改<xi-image>进度条样式

--- pages/image/index.js
+++ pages/image/index.js
@@ -3,7 +3,7 @@ Page({
 
   data: {
     uploadUrl: 'https://xmp.ctbls.com/wda/Home/uploadImg',
-    // imgs: 'https://xmp.ctbls.com/uploads/sm/voucher/20190401/331360bb0fbc034da4c0edd7bee30850.png'
+    img: 'https://xmp.ctbls.com/uploads/sm/voucher/20190401/331360bb0fbc034da4c0edd7bee30850.png',
     imgs: ''
   },
 
