cff14d049d5a167054ab66cefb4702de0a3db79e
815828396 <815828396@qq.com>
2019-04-02 13:50:15 +0800

    A：新增<province>省份选择器

--- public/components/province/index.js
+++ public/components/province/index.js
@@ -12,15 +12,30 @@ Component({
   externalClasses: ["xi-class"],
 
   properties: {
+    // 默认展示值
     provinceText: String,
     cityText: String,
     areaText: String,
+
+    // 省市区层级
+    zIndex: {
+      type: String,
+      value: 3
+    }
   },
 
   data: {
     // 优先展示默认传入值
+    isDefault: false,
+
+    // Is show Picker
     visiable: false,
 
+    // picker列表数据
+    pickerList: [],
+    value: [],
+
+    // 省市区数据
     province: [],
     city: [],
     area: [],
@@ -29,34 +44,83 @@ Component({
   },
 
   ready () {
+    this.data.isDefault = this.data.provinceText && this.data.cityText && this.data.areaText;
     this._initProvince();
   },
   methods: {
-    asd () {
-      console.log(12);
-    },
     handlePickerView ({ detail }) {
-      this.setData({ visiable: true })
+      this.setData({ visiable: true, isDefault: false });
+    },
+    pickerChange ({ detail }) {
+      const _index = detail.value[0];
+
+      // 用于：获取城市时的赋值
+      this.data.index = [_index, 0, 0];
+      this._initCity();
+
+      // 用于：修改picker-view时的赋值
+      this.setData({ index: detail.value });
     },
     // 获取省份数据
     _initProvince () {
       $get(env_provice).then(res => {
-        
         this.setData({ province: res.data });
-        console.log(res)
+
+        // 首次加载,自动获取城市信息数据
+        if (this.data.index[0] === 0) this._initCity();
       });
     },
     // 根据省份ID获取市级数据
     _initCity () {
+      const pro_id = this.data.province[this.data.index[0]].id;
+      const id = this.data.isDefault
+              && this.data.provinceText
+              && this.updPickerData('province', 'name', 0)
+              || pro_id;
+      const data = { id };
+      
       $get(env_city, data).then(res => {
-        console.log(res)
+        this.setData({ city: res.data });
+
+        this._initArea();
       });
     },
     // 根据市区ID获取地区数据
     _initArea () {
+      const _city = this.data.city[this.data.index[1]];
+      // 防止：当前省份中城市的索引值, 在新省份的城市中不存在索引而报错
+      // 如： 河北：(石家庄[0],唐山[1]) =》 北京：(北京市[0])
+      const city_id = _city && _city.id || this.data.city[0].id;
+      const id = this.data.isDefault
+              && this.data.cityText
+              && this.updPickerData('city', 'name', 1)
+              || city_id;
+      const data = { id };
+
       $get(env_area, data).then(res => {
-        console.log(res)
+        this.setData({ area: res.data });
+
+        this.data.isDefault && this.updPickerData('area', 'name', 2);
       })
+    },
+    /**
+     * 修改picker-view数据
+     * @param {String} arrKey 当前数组在data中的字符串名字
+     * @param {String} key 匹配数组中的 Key 值
+     * @param {Number} _index 需要修改的 index 中的索引值
+     */
+    updPickerData (arrKey, key, _index) {
+      const { item, index } = this.findItem({
+        array: this.data[arrKey],
+        key: key,
+        val: this.data[`${arrKey}Text`]
+      });
+
+      // 设置 页面地区文字动态改变
+      this.data.index[_index] = index;
+      this.setData({ index: this.data.index });
+
+      return item.id;
     }
   }
 })
--- utils/behaviors/behaviors.js
+++ utils/behaviors/behaviors.js
@@ -17,11 +17,30 @@ export default Behavior({
     // 获取页面数据, 默认当前页(0 || 1)  上一页(2) 依次累加
     getPage (current = 0) {
       const pages = getCurrentPages();
-      return pages[pages.length - (current ? current : 1)]
+      return pages[pages.length - (current ? current : 1)];
     },
     // 提交事件
     trigger (event, arg) {
-      this.triggerEvent(event, arg)
+      this.triggerEvent(event, arg);
+    },
+    /**
+     * 查找数组中对应中的值
+     * @param {Array} array 查找的数组
+     * @param {String} key 数组对象中的 Key
+     * @param {String} val 数组对象中 Val
+     * @returns {Object} [item: 符合条件的对象, index: 索引值]
+     */
+    findItem ({ array, key, val }) {      
+      let _index, _item;
+
+      array.forEach((item, index) => {
+        if (item[key] === val) {
+          _index = index;
+          _item = item;
+        }
+      });
+      
+      return { item: _item, index: _index }
     }
   }
 })
