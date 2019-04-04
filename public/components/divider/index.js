0db8e9c167c17b94e4ca36e122e8f27750260b8f
815828396 <815828396@qq.com>
2019-04-04 14:31:32 +0800

    A: 新增<xi-divider>分割线

---
+++ public/components/divider/index.js
@@ -0,0 +1,14 @@
+Component({
+  externalClasses: ["xi-class"],
+
+  properties: {
+    lineColor: {
+      type: String,
+      value: '#969BA5'
+    },
+    color: {
+      type: String,
+      value: '#969BA5'
+    }
+  }
+})
